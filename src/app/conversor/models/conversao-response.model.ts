ecf6f51c89e02a91012cef82d3363aeec86816a8
unknown <Cielo@STALXC037260-W.stefanini.dom>
2018-12-07 10:03:23 -0200

    Insercao do projeto

---
+++ src/app/conversor/models/conversao-response.model.ts
@@ -0,0 +1,9 @@
+export class ConversaoResponse {
+
+    constructor(
+        public base: string,
+        public date: string,
+        public rates: any) {}
+}
+
+//{"base":"USD","date":"2018-12-04","rates":{"BRL":3.8401}}
